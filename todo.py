e2a95ba78ee994b2e1f62a8215bf08769ab77992
Ay2manYT <aybaa005@osloskolen.no>
2026-03-23 10:19:43 +0100

    Lagde en kort html, og py kode

--- todo.py
+++ todo.py
@@ -1,26 +1,7 @@
 from fastapi import FastAPI
-import json, os
 
 app = FastAPI()
-FILE = "todos.json"
 
-def read():
-    if not os.path.exists(FILE):
-        return []
-    return json.load(open(FILE))
-
-def write(data):
-    json.dump(data, open(FILE, "w"), indent=2)
-
-@app.get("/todos")
-def get_todos():
-    return read()
-
-@app.post("/todos")
-def create(todo: dict):
-    data = read()
-    todo["id"] = len(data) + 1
-    todo["tasks"] = []
-    data.append(todo)
-    write(data)
-    return todo
+@app.get("/test")
+def test():
+    return {"message": "Hei fra serveren"}
