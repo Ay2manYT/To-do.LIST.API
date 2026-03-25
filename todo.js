5f6523b8e00e8f292c494ce7508ba44226c17ef3
Ay2manYT <aybaa005@osloskolen.no>
2026-03-25 21:44:40 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,12 +1,12 @@
 function visOpprettNotat() {
-  alert("Her kan du lage notater! (Fiks senere med API)");
+  alert("Her kan du lage notater. (Fiks senere med API)");
 }
 
 function visOpprettListe() {
-  alert("Her kan du lage todo-lister! (Fiks senere med API)");
+  alert("Her kan du lage todo-lister. (Fiks senere med API)");
 }
 
-// Legg til ny oppgave i todo-liste
+// Legg til ny oppgave i todo-liste 
 function leggTilOppgave() {
   const oppgave = document.getElementById("ny-oppgave").value;
   if (!oppgave) return;
